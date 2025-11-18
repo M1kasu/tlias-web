8619f7526bb96e174d4628db506963e30018edf4
yapeng <1967458972@qq.com>
2025-11-18 15:32:46 +0800

    首次提交

---
+++ src/main/java/com/itheima/controller/DeptController.java
@@ -0,0 +1,31 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Dept;
+import com.itheima.pojo.Result;
+import com.itheima.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 部门管理控制器
+ */
+@RestController
+public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    /**
+     * 查询部门列表
+     */
+    @GetMapping("/depts")
+    public Result list(){
+        List<Dept> deptList = deptService.findAll();
+        return Result.success(deptList);
+    }
+}
